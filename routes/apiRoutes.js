2896c975a3a35199e1fda998a54bd0f2e319b25d
JadeP <jnporter.jp@gmail.com>
2021-03-29 23:54:04 -0700

    final changes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,54 +1,44 @@
-const fs = require('fs')
-let tableData = require('../db/db.json')
-
+const fs = require("fs");
+let tableData = require("../db/db.json");
+​
 function noteIds() {
   tableData.forEach((newNote, i) => {
     newNote.id = i++;
-  })
+  });
 }
-
-module.exports = function(app) {
+​
+module.exports = function (app) {
+  // API GET Requests
+  app.get("/api/notes", function (req, res) {
+    fs.readFile("db/db.json", "utf8", function(err, tableData ) {
+      res.json(tableData);
+    })
+     
+  });
+​
+  // API POST Requests
+  app.post("/api/notes", function (req, res) {
+    const newNote = req.body;
+    res.redirect("/")
+​
+  });
+  // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+  // It will do this by sending out the value "true" have a table
+  // req.body is available since we're using the body parsing middleware
   
-  fs.readFile('db/db.json', JSON.stringify(tableData))
-    res.json(tableData);
-  
-    // API GET Requests
-    app.get('/api/notes', function(req, res) {
-    res.json(tableData)
-    });
-  
-    // API POST Requests
-    app.post('/api/notes', function(req, res) {
-      const newNote = req.body;
-      tableData.push(newNote);
-      noteIds()
-      fs.writeFileSync('./db/db.json', JSON.stringify(tableData))
-      res.json(tableData)
-    });
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
-      if (tableData.length < 5) {
-        tableData.push(req.body);
-        res.json(true);
-      }
-      else {
-        waitListData.push(req.body);
-        res.json(false);
-      }
-    app.delete('/api/notes/:id', function(req, res) {
-      const noteNum = req.params.id
-      for (var i = 0; i < tableData.length; i++) {
+  app.delete("/api/notes/:id", function (req, res) {
+    const noteNum = req.params.id;
+    for (var i = 0; i < tableData.length; i++) {
       if (tableData[i].id == noteNum) {
         // Empty out the arrays of data
         tableData.length = 0;
         waitListData.length = 0;
         res.json({ ok: true });
-        console.log("Deleted note with id "+req.params.id);
-        noteIds()
-        }
+        console.log("Deleted note with id " + req.params.id);
+        noteIds();
       }
-      fs.writeFileSync('./db/db.json', JSON.stringify(tableData))
-      res.json(tableData)
-    }); 
-}
+    }
+    fs.writeFileSync("./db/db.json", JSON.stringify(tableData));
+    res.json(tableData);
+  });
+};
--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 const express = require('express');
+var path = require('path');
 var htmlRoutes = require('./routes/htmlRoutes')
-
+const apiRoutes = require('./routes/apiRoutes')
+​
 // Tells node that we are creating an "express" server
 const app = express();
 // Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8080;
-
+​
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-app.use("/", htmlRoutes);
-
-// The below points our server to a series of "route" files.
-require('./routes/apiRoutes')(app)
-require('./routes/htmlRoutes')(app)
-
+app.use('/api', apiRoutes);
+​
+app.get('/notes', function(req, res) {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+​
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+​
 // The below code effectively "starts" our server
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
